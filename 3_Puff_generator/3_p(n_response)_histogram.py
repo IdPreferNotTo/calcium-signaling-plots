c00f630b0e9f119f7822cce5f3f367086df8bfbd
SidSloth <ramlowlukas@gmail.com>
2021-11-18 12:03:58 +0100

    Generalized plot for probability of open channel per puff of puff generator

---
+++ 3_Puff_generator/3_p(n_response)_histogram.py
@@ -0,0 +1,37 @@
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+
+import styles as st
+
+if __name__ == "__main__":
+    nmax_channel = 7
+    st.set_default_plot_style()
+    fig = plt.figure(tight_layout=True, figsize=(3, 4))
+    gs = gridspec.GridSpec(nmax_channel, 1)
+    axis = []
+    for i in range(nmax_channel):
+        ax = fig.add_subplot(gs[i])
+        uni_data = np.random.randint(1, nmax_channel+1-i, 200)
+        for k in range(1, nmax_channel+1-i):
+            n = 0
+            for x in uni_data:
+                if x == k:
+                    n+=1
+            ax.bar(k, n/200, 0.8, align="center", color=st.colors[0])
+
+        ax.set_xticklabels([])
+        ax.set_xticks(range(1, nmax_channel+1))
+        ax.set_xlim([0, nmax_channel+1])
+        ax.text(1, 0.5, f"$n={nmax_channel-i:d}$", va="center", rotation="vertical", transform=ax.transAxes)
+        axis.append(ax)
+
+    st.remove_top_right_axis(axis)
+    axis[int(nmax_channel/2)].set_ylabel(r"$P(n_{\rm opn})$")
+    axis[-1].set_xticklabels(range(1, nmax_channel+1))
+    axis[-1].set_xlabel(r"$n_{\rm opn}$")
+    home = os.path.expanduser("~")
+    #plt.savefig(
+    #    home + "/Data/Calcium/Plots/histo_responding_channel.pdf", transparent=True)
+    plt.show()
